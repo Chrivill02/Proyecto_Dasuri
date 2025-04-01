744766aa80e39dd309ac62edccfd48bbbf4cdecc
Fernando Contreras <fernandomorann999@gmail.com>
2025-03-31 23:40:13 -0600

    Terminando CRUD

--- nextjs-mysql-dashuri/src/app/api/inventario/[id]/route.js
+++ nextjs-mysql-dashuri/src/app/api/inventario/[id]/route.js
@@ -1,13 +1,77 @@
 import { NextResponse } from "next/server";
+import { pool } from "@/libs/mysql";
 
-export function GET(){
-	return NextResponse.json({message:'Hola buscando por id'});
+export async function GET(request, { params }) {
+  try {
+    const result = await pool.query("SELECT * FROM producto WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.length === 0) {
+      return NextResponse.json({ message: "Producto no encontrado" });
+    }
+    return NextResponse.json({ result: result }, { status: 404 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 
-
-export function DELETE(){
-	return NextResponse.json({message:'Hola eliminando por id'});
+export async function DELETE(request, { params }) {
+  try {
+    const result = await pool.query("DELETE FROM producto WHERE id = ?", [
+      params.id,
+    ]);
+    if (result.affectedRows === 0) {
+      return NextResponse.json(
+        { message: "Producto no encontrado" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ message: "Producto eliminado", result: result });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
-export function PUT(){
-	return NextResponse.json({message:'Hola actualizando por id'});
+
+export async function PUT(request, { params }) {
+  try {
+    const data = await request.json();
+    const result = await pool.query("UPDATE producto SET ? WHERE id = ? ", [
+      data,
+      params.id,
+    ]);
+    if (result.affectedRows === 0) {
+      return NextResponse.json(
+        { message: "Producto no encontrado" },
+        { status: 404 }
+      );
+    }
+    const updatedProducto = await pool.query(
+      "SELECT * FROM producto WHERE id = ? ",
+      [params.id]
+    );
+    return NextResponse.json(updatedProducto[0]);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
--- nextjs-mysql-dashuri/src/app/api/inventario/route.js
+++ nextjs-mysql-dashuri/src/app/api/inventario/route.js
@@ -2,10 +2,35 @@ import { NextResponse } from "next/server";
 import { pool } from "@/libs/mysql";
 
 export async function GET(){
-	const result = await pool.query('SELECT NOW()')
-	return NextResponse.json({message:result[0]['NOW()']});
+	try {
+		const result = await pool.query('SELECT * FROM producto')
+		return NextResponse.json(result);
+	} catch (error) {
+		console.log(error);	
+		return NextResponse.json({message: error.message},{status: 500});
+	}
 }
 
-export function POST(){
-	return NextResponse.json({message:'Hola creando producto'});
+export async function POST(request){
+	try {
+		const {nombre,stock,precio,categoria_id} = await request.json()
+		const result = await pool.query('INSERT INTO producto SET?',{
+			nombre: nombre,
+			stock: stock,
+			precio: precio,
+			categoria_id: categoria_id,
+		})
+
+		console.log(result);
+		return NextResponse.json({
+			nombre,
+			stock,
+      precio,
+      categoria_id,
+      id: result.insertId,
+		});
+	} catch (error) {
+		console.log(error);	
+		return NextResponse.json({message: error.message},{status: 500});
+	}
 }
