8ad1f92b6c09ca8564377816530f9c75da4bf54c
R.Guti <124416087+RaulvGuti@users.noreply.github.com>
2025-04-02 13:07:29 -0600

    Conexion
    En teoria ya esta mi conexion

--- nextjs-mysql-dashuri/src/libs/mysql.js
+++ nextjs-mysql-dashuri/src/libs/mysql.js
@@ -3,9 +3,9 @@ import mysql from 'serverless-mysql'
 export const pool = mysql({
 	config:{
 		host: "localhost",
-		port: 3307,
-    user: "root",
-    password: "",
+		port: 3306,
+    user: "RGuti",
+    password: "Pollito12",
     database: "dashuri"
 	}
 })
