b3480ea993b77d9efd682886b217dedbc3f73d46
Christian Villegas <99997132+Chrivill02@users.noreply.github.com>
2025-04-28 10:34:33 -0600

    Comprobacion nivel inventario

--- nextjs-mysql-dashuri/src/app/inventario/page.jsx
+++ nextjs-mysql-dashuri/src/app/inventario/page.jsx
@@ -1,10 +1,27 @@
-import ButtonBusqueda from "./buttonBus";
+'use client';
+import { useEffect, useState } from 'react';
+import ButtonBusqueda from './buttonBus';
 
-async function ViewInventario() {
+export default function ViewInventario() {
+    const [esAdmin, setEsAdmin] = useState(false);
 
-  return (
-    <ButtonBusqueda/>
-  );
+    // Verificar el nivel del usuario en localStorage
+    useEffect(() => {
+        const usuarioNivel = localStorage.getItem('usuarioNivel');
+        if (usuarioNivel && parseInt(usuarioNivel) === 1) {
+            setEsAdmin(true);
+        } else {
+            setEsAdmin(false);
+        }
+    }, []);
+
+    if (!esAdmin) {
+        return <p>No tienes acceso a esta página.</p>;
+    }
+
+    return (
+        <div>
+            <ButtonBusqueda />
+        </div>
+    );
 }
-
-export default ViewInventario;
