c5acb94ac5d352d3b453d3334b116335f9f81980
Fernando Contreras <fernandomorann999@gmail.com>
2025-04-30 11:35:36 -0600

    agregando cambio carpeta

--- nextjs-mysql-dashuri/src/libs/mysql.js
+++ nextjs-mysql-dashuri/src/libs/mysql.js
@@ -3,7 +3,7 @@ import { createPool } from 'mysql2/promise';
 export const pool = createPool({
   host: "localhost",
   user: "root",
-  password: "bd1234",
+  password: "jksdjfk",
   database: "dashuri",
   waitForConnections: true,
   connectionLimit: 10,
