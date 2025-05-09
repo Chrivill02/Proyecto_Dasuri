7af09ae1a846e0d7ece753f52bf793ba88b11b2e
ANBite <124651559+ANBite@users.noreply.github.com>
2025-05-09 09:07:06 -0600

    Se modificó la BD para compras al proveedor

--- nextjs-mysql-dashuri/src/app/compraproveedor/page.jsx
+++ nextjs-mysql-dashuri/src/app/compraproveedor/page.jsx
@@ -1,12 +1,8 @@
 import comprarAproveedor from "@/components/ComprarAProveedor";
-import botonMostrarCompras from "@/components/Boton_MostrarCompras";
-import botonMostrarProveedor from "@/components/Boton_MostrarProveedor";
 import comprasform from "@/components/CompraFactura";
 
 const Form3 = comprarAproveedor
 const Form4 = comprasform
-const Boton1 = botonMostrarCompras
-const Boton2 = botonMostrarProveedor
 
 
 function compraproveedor() {
@@ -21,8 +17,6 @@ function compraproveedor() {
 		}}> 
 			<Form3 />
 			<Form4 />
-			<Boton1 />
-			<Boton2 />
 		</div>
 	);
 }
